c1b4a0bffd2120b6154f0f339111d65fa3009e6b
Olga Bel <olza91@mail.ru>
2023-01-19 19:19:11 +0300

    5

---
+++ t3.py
@@ -0,0 +1,11 @@
+# Создайте программу для игры в ""Крестики-нолики"".
+
+# f = open('ttt.txt', 'r') #открываем файл
+# data = f.read().split()
+# f.close()
+
+import os
+os.system('cls||clear')
+
+print("Игра в 'Крестики-нолики'")
+
